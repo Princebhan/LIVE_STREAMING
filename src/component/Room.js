9fc5be324fa67e620462b6cbcd5715a864aa0f77
Prince22 <112156035+Princebhan@users.noreply.github.com>
2023-12-13 13:16:14 +0530

    last

---
+++ src/component/Room.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+function Room() {
+  const { roomId } = useParams();
+
+  function randomID(len) {
+    let result = '';
+    if (result) return result;
+    var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+      maxPos = chars.length,
+      i;
+    len = len || 5;
+    for (i = 0; i < len; i++) {
+      result += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return result;
+  }
+
+  const myMeeting = async (element) => {
+    const appID = 35454449;
+    const serverSecret = "338ab7cb3e99e093ae4fb23d6edbbe70";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomId,
+      randomID(5),
+      randomID(5)
+    );
+
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: 'Copy Link',
+          //url: `http://localhost:3000/room/${roomId}`
+          url: `https://livestreaming-rhr9.onrender.com/room/${roomId}`
+          
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
+  };
+
+  const myMeetingRef = useRef();
+
+  useEffect(() => {
+    myMeeting(myMeetingRef.current);
+  }, [roomId]);
+
+  return (
+    <div ref={myMeetingRef}>
+      {/* Your component content */}
+    </div>
+  );
+}
+
+export default Room;
